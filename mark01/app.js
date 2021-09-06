cddbef6827415e9972504b9394f9bb560e5fd4ff
manujagobind <manujagobind@gmail.com>
2021-09-06 22:07:23 +0530

    ex10 - ex11 (arrays)

--- mark01/app.js
+++ mark01/app.js
@@ -1,50 +1,7 @@
-console.log("Printing my name 5 times:");
+// An array
+var groceryList = ["Potato", "Tomato", "Peas"];
 
-// A for loops
-for (var count = 0 ; count < 5 ; count++) {
-    console.log("Gobind");
-}
-
-/*
-
-Star pattern 1
-
-*
-* *
-* * *
-* * * *
-* * * * *
-
-*/
-
-console.log("Start pattern 1:");
-
-for (var row = 0 ; row < 5 ; row++) {
-    var output = "";
-    for (var column = 0 ; column <= row ; column++) {
-        output += "* ";
-    }
-    console.log(output);
-}
-
-/*
-
-Star pattern 2
-
-* * * * *
-* * * *
-* * *
-* *
-*
-
-*/
-
-console.log("Start pattern 2:");
-
-for (var row = 5 ; row > 0 ; row--) {
-    output = "";
-    for (var column = row ; column >= 1 ; column--) {
-        output += "* ";
-    }
-    console.log(output);
+// Iterate over the array
+for (var index = 0 ; index < groceryList.length ; index++) {
+    console.log(groceryList[index])
 }

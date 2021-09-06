5a4df3dd0d7a41e3cdf27466533c07115d463e58
manujagobind <manujagobind@gmail.com>
2021-09-06 22:31:34 +0530

    ex12 - ex15

--- mark01/app.js
+++ mark01/app.js
@@ -1,7 +1,38 @@
-// An array
-var groceryList = ["Potato", "Tomato", "Peas"];
+// Imports
+var readLineSync = require('readline-sync');
 
-// Iterate over the array
-for (var index = 0 ; index < groceryList.length ; index++) {
-    console.log(groceryList[index])
+// Global variables
+var score = 0;
+
+// Function definition
+function play(question, answer) {
+    var userAnswer = readLineSync.question(question);
+
+    if (userAnswer === answer) {
+        score = score + 1;
+        console.log("You are right!");
+    } else {
+        score = score - 1;
+        console.log("You are wrong!");
+    }
 }
+
+// Array of questions
+var questions = [
+    {
+        question: "In which year did India gain freedom?",
+        answer: "1947"
+    },
+    {
+        question: "What is the capital of Tamil Nadu?",
+        answer: "Chennai"
+    }
+]
+
+// Iterate over all questions
+for (var index = 0 ; index < questions.length ; index++) {
+    play(questions[index].question, questions[index].answer);
+}
+
+// Display the result
+console.log("Your score is " + score);

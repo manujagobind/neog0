4e36f5714d1a4e8404d946adef156366c38ede6d
manujagobind <manujagobind@gmail.com>
2021-09-06 21:46:01 +0530

    ex09 - for loop

--- mark01/app.js
+++ mark01/app.js
@@ -1,25 +1,50 @@
-// Imports
-var readLineSync = require('readline-sync');
+console.log("Printing my name 5 times:");
 
-// Global variables
-var score = 0;
-
-// Function definition
-function play(question, answer) {
-    var userAnswer = readLineSync.question(question);
-
-    if (userAnswer === answer) {
-        score = score + 1;
-        console.log("You are right!");
-    } else {
-        score = score - 1;
-        console.log("You are wrong!");
-    }
+// A for loops
+for (var count = 0 ; count < 5 ; count++) {
+    console.log("Gobind");
 }
 
-// Calling the function
-play("In which year did India gain freedom? ", "1947")
-play("What is the capital of Tamil Nadu? ", "Chennai")
+/*
 
-// Display
-console.log("Your score is " + score);
+Star pattern 1
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+console.log("Start pattern 1:");
+
+for (var row = 0 ; row < 5 ; row++) {
+    var output = "";
+    for (var column = 0 ; column <= row ; column++) {
+        output += "* ";
+    }
+    console.log(output);
+}
+
+/*
+
+Star pattern 2
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+console.log("Start pattern 2:");
+
+for (var row = 5 ; row > 0 ; row--) {
+    output = "";
+    for (var column = row ; column >= 1 ; column--) {
+        output += "* ";
+    }
+    console.log(output);
+}

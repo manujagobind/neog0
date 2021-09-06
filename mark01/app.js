7c7ccb6c63ec2106e2f0824443457ff6b8cb4f35
manujagobind <manujagobind@gmail.com>
2021-09-06 21:11:16 +0530

    ex05 - ex06

--- mark01/app.js
+++ mark01/app.js
@@ -1,12 +1,22 @@
 // Imports
 var readLineSync = require('readline-sync');
 
+// Variables
+var score = 0;
+
 // User input
-var userName = readLineSync.question("What is your name? ");
+var userAns = readLineSync.question("Are you older than 25? ");
 
-// String concatenation
-var welcomeMessage = "Hi, " + userName + "! Welcome to the CLI app!";
 
-// Display
-console.log(welcomeMessage);
+console.log("You entered " + userAns)
 
+// App logic
+if (userAns === "yes") {
+    score = score + 1;
+    console.log("You are right!");
+} else {
+    score = score - 1;
+    console.log("You are wrong");
+}
+
+console.log("You score is " + score);

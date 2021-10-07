38094b72ffb61211c51a90f80507964245af6614
manujagobind <manujagobind@gmail.com>
2021-10-07 22:51:57 +0530

    Lesson 04 ex03: handle button click

--- mark06/script.js
+++ mark06/script.js
@@ -1,3 +1,13 @@
-var value = prompt("Enter something: ");
+BUTTON_SELECTOR = "#btn";
 
-alert("You entered: " + value);
+var buttonElement = document.querySelector(BUTTON_SELECTOR);
+
+function handleButtonClick() {
+    alert("Button was clicked!");
+}
+
+if (buttonElement != null) {
+    buttonElement.addEventListener("click", handleButtonClick);
+} else {
+    console.log("No button found using selector " + BUTTON_SELECTOR);
+}

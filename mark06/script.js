f080df87a095b0a67e0a26ce35b109d1f90f5780
manujagobind <manujagobind@gmail.com>
2021-10-07 23:28:10 +0530

    Lesson 04 ex05: display o/p in a div

--- mark06/script.js
+++ mark06/script.js
@@ -1,8 +1,15 @@
 BUTTON_SELECTOR = "#btn";
 TEXT_SELECTOR = "#text";
+OUTPUT_DIV_SELECTOR = "#output";
+
+STYLE_COLOR_SUCCESS = "green";
+STYLE_COLOR_ERR = "red";
+STYLE_MARGIN = "10px"
 
 var buttonElement = document.querySelector(BUTTON_SELECTOR);
 var textAreaElement = document.querySelector(TEXT_SELECTOR);
+var outputDivElement = document.querySelector(OUTPUT_DIV_SELECTOR);
+
 var textAreaContent = null;
 
 function handleButtonClick() {
@@ -11,10 +18,25 @@ function handleButtonClick() {
         textAreaContent = textAreaElement.value;
     }
 
-    var displayText = textAreaContent != null && textAreaContent != "" ? 
-        "You have entered: " + textAreaContent : "You did not enter any text.";
+    var isTextNullOrEmpty = textAreaContent == null || textAreaContent == "";
 
-    alert(displayText);
+    var displayText = !isTextNullOrEmpty ? 
+        "You have entered: " + textAreaContent : 
+        "You did not enter any text. Please try again.";
+
+    if (outputDivElement != null) {
+
+        outputDivElement.style.marginTop = STYLE_MARGIN;
+        if (isTextNullOrEmpty) {
+            outputDivElement.style.color = STYLE_COLOR_ERR;
+        } else {
+            outputDivElement.style.color = STYLE_COLOR_SUCCESS;
+        }
+
+        outputDivElement.innerText = displayText;
+    } else {
+        alert(displayText);
+    }
 }
 
 if (buttonElement != null) {

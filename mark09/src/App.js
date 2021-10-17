89180dcaba11d10bd3286f6d805cd2f9f014a262
manujagobind <manujagobind@gmail.com>
2021-10-17 16:20:43 +0530

    Create app components and app data

--- mark09/src/App.js
+++ mark09/src/App.js
@@ -1,9 +1,61 @@
 import { render } from 'react-dom';
 
-function App() {
+import './App.css';
+
+var appData = {
+    "javascript": [
+        {
+            title: "Eloquent JavaScript",
+            rating: 4
+        },
+        {
+            title: "You Don't Know JS",
+            rating: 3.5
+        }
+    ],
+    "fiction": [
+        {
+            title: "Shiva Triology",
+            rating: 5
+        },
+        {
+            title: "Harry Potter and the Sorcerer's Stone",
+            rating: 5
+        }
+    ],
+    "business": [
+        {
+            title: "Never Split the Difference",
+            rating: 3.5
+        },
+        {
+            title: "Loonsots",
+            rating: 5
+        }
+    ]
+};
+
+function BookInfo(props) {
     return (
         <div>
-            <header>📚 goodbooks</header>
+            <h2>{props.title}</h2>
+            <p>{props.rating}/5</p>
+        </div>
+    );
+}
+
+function BookList(props) {    
+    return (
+        props.books.map( (book) => {
+            <BookInfo title={book.title} rating={book.rating} />
+        })
+    );
+}
+
+function App() {
+    return (
+        <div className="app-container">
+            <header>📚 goodbooks</header>            
         </div>
     );
 }

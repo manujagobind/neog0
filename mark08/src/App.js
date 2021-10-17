def46e071f1efef91108dabb7ea42a69b10999a3
manujagobind <manujagobind@gmail.com>
2021-10-17 13:35:56 +0530

    ex05 - jsx variables, eval

--- mark08/src/App.js
+++ mark08/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+var username = "Gobind";
+var textColor = "blue";
+
 function App() {
     return (
         <div>
             <header>Inside Outttt</header>
+            <h1 style={{color: textColor}}>Welcome {username}</h1>
         </div>
     );
 }

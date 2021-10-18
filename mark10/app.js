1a7bbc3c46e6cd972a0eab2d81e176cd82340f5e
manujagobind <manujagobind@gmail.com>
2021-10-18 21:35:55 +0530

    Hide second input until first is provided

--- mark10/app.js
+++ mark10/app.js
@@ -1,5 +1,6 @@
 const ID_BILl_AMT = "bill-amt";
 const ID_CASH_AMT = "cash-amt";
+const ID_CASH_AMT_LABEL = "cash-amt-label";
 const ID_BTN_SUBMIT = "submit";
 const ID_ERR_TEXT = "error";
 const ID_CELL_PREFIX = "change-"
@@ -8,10 +9,14 @@ const ERR_BILL_AMT_INVALID = "Bill amount must be a positive integer.";
 const ERR_CASH_AMT_INVALID = "Cash amount must be a positive integer.";
 const ERR_CASH_AMT_NOT_SUFFICIENT = "Cash amount is not sufficient."
 
+const STYLE_DISPLAY_BLOCK = "block";
+const STYLE_DISPLAY_NONE  = "none";
+
 const DENOMINATION = [2000, 500, 100, 20, 10, 5, 1];
 
 var billAmtInputElement = document.getElementById(ID_BILl_AMT);
 var cashAmtInputElement = document.getElementById(ID_CASH_AMT);
+var cashAmtLabelElement = document.getElementById(ID_CASH_AMT_LABEL);
 var submitButtonElement = document.getElementById(ID_BTN_SUBMIT);
 var errorTextElement    = document.getElementById(ID_ERR_TEXT);
 
@@ -77,7 +82,7 @@ function displayReturnChange(returnChange) {
     }
 }
 
-function clearReturnChange() {
+function clearReturnChangeDisplay() {
     for (var denomIndex = 0 ; denomIndex < DENOMINATION.length ; denomIndex++) {
         // <td id="change-xx"></td>
         var tableDataElement = document.getElementById(ID_CELL_PREFIX + DENOMINATION[denomIndex]);
@@ -89,7 +94,7 @@ function clearReturnChange() {
 
 function handleSubmit() {
     clearErrorDisplay();
-    clearReturnChange();
+    clearReturnChangeDisplay();
 
     var billAmount = Number(billAmtInputElement.value);
     var cashAmount = Number(cashAmtInputElement.value);
@@ -99,9 +104,27 @@ function handleSubmit() {
     }
 }
 
+function handleInputBillAmt(event) {
+    var input = event.target.value;
+
+    if (doesNotExist(input)) {
+        return;
+    }
+
+    if (input.trim().length > 0) {
+        cashAmtLabelElement.style.display = STYLE_DISPLAY_BLOCK;
+        cashAmtInputElement.style.display = STYLE_DISPLAY_BLOCK;
+    } else {
+        cashAmtLabelElement.style.display = STYLE_DISPLAY_NONE;
+        cashAmtInputElement.style.display = STYLE_DISPLAY_NONE;
+        cashAmtInputElement.value = null;
+    }
+}
+
 if (doesNotExist(billAmtInputElement) || doesNotExist(cashAmtInputElement) ||
     doesNotExist(submitButtonElement)) {
     alert("Page not set correctly.");
 }
 
+billAmtInputElement.addEventListener("input", handleInputBillAmt);
 submitButtonElement.addEventListener("click", handleSubmit);

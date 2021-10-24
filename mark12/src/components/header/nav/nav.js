f97acd81c49e4e9b69f83c42417513f1253bbe2a
manujagobind <manujagobind@gmail.com>
2021-10-24 13:11:18 +0530

    fix: active nav was incorrect when non-root  was accessed directly

--- mark12/src/components/header/nav/nav.js
+++ mark12/src/components/header/nav/nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import './nav.css';
 
@@ -8,8 +8,20 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeNavIndex: 0
+            activeNavIndex: this.getNavIndexFromPath(this.props.location.pathname)
         }
+    }
+
+    getNavIndexFromPath(path) {
+        console.log(path)
+        var navIndex = 0;
+        for (var index = 0 ; index < this.props.nav.length ; index++) {            
+            if (this.props.nav[index].route === path) {
+                navIndex = index;
+                break;
+            }
+        }
+        return navIndex;
     }
 
     toggleNav(navIndex) {
@@ -35,4 +47,4 @@ class Navbar extends React.Component {
     }
 }
 
-export default Navbar;
+export default withRouter(Navbar);

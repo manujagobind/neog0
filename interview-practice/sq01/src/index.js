b4151491a28d05da5c41d5718dbd51af90e63ecd
manujagobind <manujagobind@gmail.com>
2021-10-30 08:17:25 +0530

    Set up base react app

---
+++ interview-practice/sq01/src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <header>
+            <h1>Sample Question 1</h1>
+        </header>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
